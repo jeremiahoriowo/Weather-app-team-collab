042d5dcd5153f76aa3d9783a465b276ab15b8b09
victusen <usenvictor40@gmail.com>
2025-09-26 16:32:05 +0100

    Created JS file and added header starter code

---
+++ index.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    extends: {
+      "neutral 900": '#03012dff',
+      "neutral 800": '#272541ff',
+      "Neutral 700": '#312f4bff',
+      "Neutral 600": '#3d3b5eff',
+      "Neutral 300": '#aeaeb7ff',
+      "Neutral 200": '#d5d4d9ff',
+      "Neutral 0": '#ffffffff'
+    }
+  }
+}
